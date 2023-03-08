8262095f6fc977c49073e4294f84f7550f0085d1
wyavern <127310258+wyavern@users.noreply.github.com>
2023-03-08 20:06:55 +0700

    Update LightItem.cs

--- Content/Items/Weapons/LightItem.cs
+++ Content/Items/Weapons/LightItem.cs
@@ -5,19 +5,16 @@ using RandomContentModIII.Content.Projectiles;
 
 namespace RandomContentModIII.Content.Items.Weapons
 {
-	/// <summary>
-	/// This weapon and its corresponding projectile showcase the CloneDefaults() method, which allows for cloning of other items.
-	/// For this example, we shall copy the Meowmere and its projectiles with the CloneDefaults() method, while also changing them slightly.
-	/// For a more detailed description of each item field used here, check out <see cref="ExampleSword" />.
-	/// </summary>
 	public class LightItem : ModItem
 	{
-		public override void SetStaticDefaults() {
+		public override void SetStaticDefaults() 
+		{
 			DisplayName.SetDefault("Light Item");
 			Tooltip.SetDefault("'Light it all up, mate!'");
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 		}
-		public override void SetDefaults() {
+		public override void SetDefaults() 
+		{
 			Item.CloneDefaults(ItemID.Arkhalis);
 			Item.UseSound = SoundID.Item1;
 			Item.shoot = ModContent.ProjectileType<LightItemBlade>();
@@ -26,7 +23,8 @@ namespace RandomContentModIII.Content.Items.Weapons
 			Item.autoReuse = true;
 			Item.useTime = 7;
 		}
-		public override void AddRecipes() {
+		public override void AddRecipes() 
+		{
 			CreateRecipe()
 				.AddIngredient(ItemID.Torch, 100)
 				.AddTile(TileID.WorkBenches)
