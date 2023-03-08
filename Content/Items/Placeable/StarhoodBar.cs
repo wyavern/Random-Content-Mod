cc0fbe56ea9cac0d80fcb925e9aa545021d6fdeb
wyavern <127310258+wyavern@users.noreply.github.com>
2023-03-08 20:01:46 +0700

    Update StarhoodBar.cs
    Made this thing more valueable than a platinum bar

--- Content/Items/Placeable/StarhoodBar.cs
+++ Content/Items/Placeable/StarhoodBar.cs
@@ -8,21 +8,21 @@ namespace RandomContentModIII.Content.Items.Placeable
 	{
 		public override void SetStaticDefaults() {
 			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 25;
-			ItemID.Sets.SortingPriorityMaterials[Item.type] = 59; // Influences the inventory sort order. 59 is PlatinumBar, higher is more valuable.
+			ItemID.Sets.SortingPriorityMaterials[Item.type] = 70; 
 		}
 
 		public override void SetDefaults() {
 			Item.width = 20;
 			Item.height = 20;
 			Item.maxStack = 999;
-			Item.value = 500000; // The cost of the item in copper coins. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
+			Item.value = 500000;
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.useTurn = true;
 			Item.useAnimation = 15;
 			Item.useTime = 10;
 			Item.autoReuse = true;
 			Item.consumable = true;
-			Item.createTile = ModContent.TileType<Tiles.StarhoodBarBlock>(); // The ID of the wall that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets)..
+			Item.createTile = ModContent.TileType<Tiles.StarhoodBarBlock>(); 
 			Item.placeStyle = 0;
 			Item.material = true;
 		}
