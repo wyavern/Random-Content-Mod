cbd1b80fc384b1b7c8aee9b88a547709898715b5
wyavern <127310258+wyavern@users.noreply.github.com>
2023-03-19 12:35:28 +0700

    Update IcedBar.cs

--- Content/Items/Placeable/IcedBar.cs
+++ Content/Items/Placeable/IcedBar.cs
@@ -15,14 +15,14 @@ namespace RandomContentModIII.Content.Items.Placeable
 			Item.width = 20;
 			Item.height = 20;
 			Item.maxStack = 999;
-			Item.value = 500000; // The cost of the item in copper coins. (1 = 1 copper, 100 = 1 silver, 1000 = 1 gold, 10000 = 1 platinum)
+			Item.value = 500000; /
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.useTurn = true;
 			Item.useAnimation = 15;
 			Item.useTime = 10;
 			Item.autoReuse = true;
 			Item.consumable = true;
-			Item.createTile = ModContent.TileType<Tiles.IcedBarBlock>(); // The ID of the wall that this item should place when used. ModContent.TileType<T>() method returns an integer ID of the wall provided to it through its generic type argument (the type in angle brackets)..
+			Item.createTile = ModContent.TileType<Tiles.IcedBarBlock>();
 			Item.placeStyle = 0;
 			Item.material = true;
 		}
