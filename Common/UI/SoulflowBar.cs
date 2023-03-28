6c23b000161caa5f388e4ebcd92876edfc054175
wyavern <127310258+wyavern@users.noreply.github.com>
2023-03-28 18:27:50 +0700

    Added a new mechanic

--- Common/UI/SoulflowBar.cs
+++ Common/UI/SoulflowBar.cs
@@ -13,8 +13,6 @@ namespace RandomContentModIII.Common.UI.ExampleResourceUI
 {
 	internal class SoulflowBar : UIState
 	{
-		// For this bar we'll be using a frame texture and then a gradient inside bar, as it's one of the more simpler approaches while still looking decent.
-		// Once this is all set up make sure to go and do the required stuff for most UI's in the ModSystem class.
 		private UIText text;
 		private UIElement area;
 		private UIImage barFrame;
@@ -22,75 +20,55 @@ namespace RandomContentModIII.Common.UI.ExampleResourceUI
 		private Color gradientB;
 
 		public override void OnInitialize() {
-			// Create a UIElement for all the elements to sit on top of, this simplifies the numbers as nested elements can be positioned relative to the top left corner of this element. 
-			// UIElement is invisible and has no padding.
+
 			area = new UIElement();
-			area.Left.Set(-area.Width.Pixels - 600, 1f); // Place the resource bar to the left of the hearts.
-			area.Top.Set(30, 0f); // Placing it just a bit below the top of the screen.
-			area.Width.Set(182, 0f); // We will be placing the following 2 UIElements within this 182x60 area.
+			area.Left.Set(-area.Width.Pixels - 600, 1f); 
+			area.Top.Set(30, 0f); 
+			area.Width.Set(182, 0f); .
 			area.Height.Set(60, 0f);
 
-			barFrame = new UIImage(ModContent.Request<Texture2D>("RandomContentModIII/Common/UI/SoulflowBarFrame")); // Frame of our resource bar
+			barFrame = new UIImage(ModContent.Request<Texture2D>("RandomContentModIII/Common/UI/SoulflowBarFrame")); 
 			barFrame.Left.Set(22, 0f);
 			barFrame.Top.Set(0, 0f);
 			barFrame.Width.Set(138, 0f);
 			barFrame.Height.Set(34, 0f);
 
-			text = new UIText("Hold or equip a soulflow item!", 0.8f); // text to show stat
+			text = new UIText("Hold or equip a soulflow item!", 0.8f); 
 			text.Width.Set(138, 0f);
 			text.Height.Set(34, 0f);
 			text.Top.Set(40, 0f);
 			text.Left.Set(0, 0f);
 
-			gradientA = new Color(84, 213, 222); // A dark purple
-			gradientB = new Color(165, 47, 204); // A light purple
-
+			gradientA = new Color(84, 213, 222);
+			gradientB = new Color(165, 47, 204); 
 			area.Append(text);
 			area.Append(barFrame);
 			Append(area);
 		}
 
-		/*public override void Draw(SpriteBatch spriteBatch) {
-			// This prevents drawing unless we are using an ExampleCustomResourceWeapon
-			//if (Main.LocalPlayer.HeldItem.ModItem is not ExampleCustomResourceWeapon)
-			//	return;
-
-			base.Draw(spriteBatch);
-		}*/
-
-		// Here we draw our UI
 		protected override void DrawSelf(SpriteBatch spriteBatch) {
 			base.DrawSelf(spriteBatch);
 
 			var modPlayer = Main.LocalPlayer.GetModPlayer<SoulflowPlayer>();
-			// Calculate quotient
-			float quotient = (float)modPlayer.SoulflowCurrent / modPlayer.SoulflowMax2; // Creating a quotient that represents the difference of your currentResource vs your maximumResource, resulting in a float of 0-1f.
-			quotient = Utils.Clamp(quotient, 0f, 1f); // Clamping it to 0-1f so it doesn't go over that.
-
-			// Here we get the screen dimensions of the barFrame element, then tweak the resulting rectangle to arrive at a rectangle within the barFrame texture that we will draw the gradient. These values were measured in a drawing program.
+			float quotient = (float)modPlayer.SoulflowCurrent / modPlayer.SoulflowMax2; 
+			quotient = Utils.Clamp(quotient, 0f, 1f); 
 			Rectangle hitbox = barFrame.GetInnerDimensions().ToRectangle();
 			hitbox.X += 12;
 			hitbox.Width -= 24;
 			hitbox.Y += 8;
 			hitbox.Height -= 16;
-
-			// Now, using this hitbox, we draw a gradient by drawing vertical lines while slowly interpolating between the 2 colors.
 			int left = hitbox.Left;
 			int right = hitbox.Right;
 			int steps = (int)((right - left) * quotient);
 			for (int i = 0; i < steps; i += 1) {
-				float percent = (float)i / steps; // Alternate Gradient Approach
-				//float percent = (float)i / (right - left);
+				float percent = (float)i / steps; // gradient that grows as the bar fills
+				//float percent = (float)i / (right - left); // usual gradient
 				spriteBatch.Draw(TextureAssets.MagicPixel.Value, new Rectangle(left + i, hitbox.Y, 1, hitbox.Height), Color.Lerp(gradientA, gradientB, percent));
 			}
 		}
 
 		public override void Update(GameTime gameTime) {
-			//if (Main.LocalPlayer.HeldItem.ModItem is not WaterWaveBook)
-			//	return;
-
 			var modPlayer = Main.LocalPlayer.GetModPlayer<SoulflowPlayer>();
-			// Setting the text per tick to update and show our resource values.
 			text.SetText($"Soulflow: {modPlayer.SoulflowCurrent} / {modPlayer.SoulflowMax2}");
 			base.Update(gameTime);
 		}
@@ -103,7 +81,6 @@ namespace RandomContentModIII.Common.UI.ExampleResourceUI
 		internal SoulflowBar SoulflowBar;
 
 		public override void Load() {
-			// All code below runs only if we're not loading on a server
 			if (!Main.dedServ) {
 				SoulflowBar = new();
 				SoulflowBarUserInterface = new();
