eba85fc83e750d7957ec7e0b363bab76c918daf4
wyavern <127310258+wyavern@users.noreply.github.com>
2023-06-15 15:44:43 +0700

    Added a new buff

---
+++ Content/Items/Other/PlasmaNucleus.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace RandomContentModIII.Content.Items.Other
+{
+	public class PlasmaNucleus : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Plasma Core");
+			Tooltip.SetDefault("Can be combined with ectoplasm to activate it.");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 28;
+			Item.height = 28;
+			//Item.useStyle = 4;
+			Item.useAnimation = 90;
+			Item.useTime = 90;
+			Item.useTurn = true;
+			//Item.UseSound = SoundID.Item92;
+			Item.maxStack = 1;
+			Item.consumable = false;
+			Item.rare = 13;
+			Item.value = Item.buyPrice(gold: 100);
+			//Item.buffType = ModContent.BuffType<Buffs.PlasmafluxBuff>(); // Specify an existing buff to be applied when used.
+			//Item.buffTime = 7200; // The amount of time the buff declared in Item.buffType will last in ticks. 5400 / 60 is 90, so this buff will last 90 seconds.
+		}
+	}
+}
