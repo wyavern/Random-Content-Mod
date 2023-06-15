09666006da291a799ad3856191b1a8e6f35c09d9
wyavern <127310258+wyavern@users.noreply.github.com>
2023-06-15 15:44:30 +0700

    Added a new boss
    Currently it's not complete, but in the future this boss will be either replaced, completed or removed

---
+++ Content/NPCs/Bosses/WormBoss.cs
@@ -0,0 +1,294 @@
+﻿﻿using RandomContentModIII.Common.Systems;
+using RandomContentModIII.Content.Items.Consumables;
+using Microsoft.Xna.Framework;
+using System.IO;
+using Terraria;
+using Terraria.GameContent.Bestiary;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.DataStructures;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RandomContentModIII.Content.NPCs.Bosses
+{
+	[AutoloadBossHead] // Looks for a .png file like "ExampleBoss_Head_Boss" and sets it as the minimap icon of the boss
+	// These three class showcase usage of the WormHead, WormBody and WormTail classes from Worm.cs
+	internal class WormBossHead : WormHead
+	{
+		public override int BodyType => ModContent.NPCType<WormBossBody>();
+
+		public override int TailType => ModContent.NPCType<WormBossTail>();
+
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Lunar Worm");
+
+			var drawModifier = new NPCID.Sets.NPCBestiaryDrawModifiers(0) { // Influences how the NPC looks in the Bestiary
+				CustomTexturePath = "RandomContentModIII/Assets/Bestiary/WormBoss_Bestiary", // If the NPC is multiple parts like a worm, a custom texture for the Bestiary is encouraged.
+				Position = new Vector2(40f, 24f),
+				PortraitPositionXOverride = 0f,
+				PortraitPositionYOverride = 12f
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(NPC.type, drawModifier);
+		}
+
+		public override void SetDefaults() {
+			// Head is 10 defence, body 20, tail 30.
+			NPC.CloneDefaults(NPCID.TheDestroyer);
+			NPC.aiStyle = -1;
+			NPC.defense = 70;
+			NPC.lifeMax = 7907905;
+			NPC.damage = 333;
+			NPC.width = 30; // The width of the npc's hitbox (in pixels)
+			NPC.height = 48; // The height of the npc's hitbox (in pixels)
+			NPC.alpha = 0;
+			NPC.value = 2500000f;
+			NPC.boss = true;
+			//CanFly = true;
+		}
+
+		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
+			// We can use AddRange instead of calling Add multiple times in order to add multiple items at once
+			bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
+				// Sets the spawning conditions of this NPC that is listed in the bestiary.
+				//BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Underground,
+				//BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions.Biomes.Caverns,
+
+				// Sets the description of this NPC that is listed in the bestiary.
+				new FlavorTextBestiaryInfoElement("It seems like a Digger can be far more dangerous than usual.")
+			});
+		}
+
+		public override void ModifyNPCLoot(NPCLoot npcLoot) {
+			// Do NOT misuse the ModifyNPCLoot and OnKill hooks: the former is only used for registering drops, the latter for everything else
+
+			// Add the treasure bag using ItemDropRule.BossBag (automatically checks for expert mode)
+			npcLoot.Add(ItemDropRule.BossBag(ModContent.ItemType<WormBossBag>()));
+
+			// Trophies are spawned with 1/10 chance
+			//npcLoot.Add(ItemDropRule.Common(ModContent.ItemType<Items.Placeable.Furniture.MinionBossTrophy>(), 10));
+
+			// ItemDropRule.MasterModeCommonDrop for the relic
+			//npcLoot.Add(ItemDropRule.MasterModeCommonDrop(ModContent.ItemType<Items.Placeable.Furniture.MinionBossRelic>()));
+
+			// ItemDropRule.MasterModeDropOnAllPlayers for the pet
+			//npcLoot.Add(ItemDropRule.MasterModeDropOnAllPlayers(ModContent.ItemType<WormBossPetItem>(), 4));
+
+			// All our drops here are based on "not expert", meaning we use .OnSuccess() to add them into the rule, which then gets added
+			LeadingConditionRule notExpertRule = new LeadingConditionRule(new Conditions.NotExpert());
+
+			// Notice we use notExpertRule.OnSuccess instead of npcLoot.Add so it only applies in normal mode
+			// Boss masks are spawned with 1/7 chance
+			//notExpertRule.OnSuccess(ItemDropRule.Common(ModContent.ItemType<MinionBossMask>(), 7));
+
+			// This part is not required for a boss and is just showcasing some advanced stuff you can do with drop rules to control how items spawn
+			// We make 12-15 ExampleItems spawn randomly in all directions, like the lunar pillar fragments. Hereby we need the DropOneByOne rule,
+			// which requires these parameters to be defined
+			//int itemType = ModContent.ItemType<ExampleItem>();
+			int itemType = ItemID.LunarBar;
+			var parameters = new DropOneByOne.Parameters() {
+				ChanceNumerator = 1,
+				ChanceDenominator = 1,
+				MinimumStackPerChunkBase = 1,
+				MaximumStackPerChunkBase = 1,
+				MinimumItemDropsCount = 12,
+				MaximumItemDropsCount = 15,
+			};
+
+			notExpertRule.OnSuccess(new DropOneByOne(itemType, parameters));
+
+			// Finally add the leading rule
+			npcLoot.Add(notExpertRule);
+		}
+
+		public override void OnKill() {
+			// This sets downedMinionBoss to true, and if it was false before, it initiates a lantern night
+			NPC.SetEventFlagCleared(ref DownedBossSystem.downedWormBoss, -1);
+
+			// Since this hook is only ran in singleplayer and serverside, we would have to sync it manually.
+			// Thankfully, vanilla sends the MessageID.WorldData packet if a BOSS was killed automatically, shortly after this hook is ran
+
+			// If your NPC is not a boss and you need to sync the world (which includes ModSystem, check DownedBossSystem), use this code:
+			/*
+			if (Main.netMode == NetmodeID.Server) {
+				NetMessage.SendData(MessageID.WorldData);
+			}
+			*/
+		}
+
+		public override void BossLoot(ref string name, ref int potionType) {
+			// Here you'd want to change the potion type that drops when the boss is defeated. Because this boss is early pre-hardmode, we keep it unchanged
+			// (Lesser Healing Potion). If you wanted to change it, simply write "potionType = ItemID.HealingPotion;" or any other potion type
+		}
+
+		public override bool CanHitPlayer(Player target, ref int cooldownSlot) {
+			cooldownSlot = ImmunityCooldownID.Bosses; // use the boss immunity cooldown counter, to prevent ignoring boss attacks by taking damage from other sources
+			return true;
+		}
+
+		public override void Init() {
+			// Set the segment variance
+			// If you want the segment length to be constant, set these two properties to the same value
+			MinSegmentLength = 40;
+			MaxSegmentLength = 40;
+
+			CommonWormInit(this);
+		}
+
+		// This method is invoked from ExampleWormHead, ExampleWormBody and ExampleWormTail
+		internal static void CommonWormInit(Worm worm) {
+			// These two properties handle the movement of the worm
+			worm.MoveSpeed = 15f;
+			worm.Acceleration = 0.545f;
+		}
+
+		private int attackCounter;
+		public override void SendExtraAI(BinaryWriter writer) {
+			writer.Write(attackCounter);
+		}
+
+		public override void ReceiveExtraAI(BinaryReader reader) {
+			attackCounter = reader.ReadInt32();
+		}
+
+		public override void AI() {
+			if (Main.netMode != NetmodeID.MultiplayerClient) {
+				if (attackCounter > 0) {
+					attackCounter--; // tick down the attack counter.
+				}
+
+				Player target = Main.player[NPC.target];
+				// If the attack counter is 0, this NPC is less than 12.5 tiles away from its target, and has a path to the target unobstructed by blocks, summon a projectile.
+				if (attackCounter <= 0 && Vector2.Distance(NPC.Center, target.Center) < 200 && Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1)) {
+					Vector2 direction = (target.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
+					direction = direction.RotatedByRandom(MathHelper.ToRadians(10));
+
+					int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 1, ProjectileID.DeathLaser, 300, 0, Main.myPlayer);
+					Main.projectile[projectile].timeLeft = 60;
+					attackCounter = 250;
+					NPC.netUpdate = true;
+				}
+			}
+		}
+	}
+
+	internal class WormBossBody : WormBody
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Lunar Worm");
+
+			NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers(0) {
+				Hide = true // Hides this NPC from the Bestiary, useful for multi-part NPCs whom you only want one entry.
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(NPC.type, value);
+		}
+
+		public override void SetDefaults() {
+			NPC.CloneDefaults(NPCID.TheDestroyerBody);
+			
+			NPC.aiStyle = -1;
+			NPC.defense = 90;
+			NPC.lifeMax = 7907905;
+			NPC.damage = 144;
+			NPC.width = 30; // The width of the npc's hitbox (in pixels)
+			NPC.height = 48; // The height of the npc's hitbox (in pixels)
+			NPC.alpha = 0;
+		}
+
+		public override void Init() {
+			WormBossHead.CommonWormInit(this);
+		}
+
+		private int attackCounter;
+		public override void SendExtraAI(BinaryWriter writer) {
+			writer.Write(attackCounter);
+		}
+
+		public override void ReceiveExtraAI(BinaryReader reader) {
+			attackCounter = reader.ReadInt32();
+		}
+
+		public override void AI() {
+			if (Main.netMode != NetmodeID.MultiplayerClient) {
+				if (attackCounter > 0) {
+					attackCounter--; // tick down the attack counter.
+				}
+
+				Player target = Main.player[NPC.target];
+				// If the attack counter is 0, this NPC is less than 12.5 tiles away from its target, and has a path to the target unobstructed by blocks, summon a projectile.
+				if (attackCounter <= 0 && Vector2.Distance(NPC.Center, target.Center) < 200 && Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1)) {
+					Vector2 direction = (target.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
+					direction = direction.RotatedByRandom(MathHelper.ToRadians(10));
+
+					int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 1, ProjectileID.DeathLaser, 95, 0, Main.myPlayer);
+					Main.projectile[projectile].timeLeft = 60;
+					attackCounter = 250;
+					NPC.netUpdate = true;
+				}
+			}
+		}
+	}
+
+	internal class WormBossTail : WormTail
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Lunar Worm");
+
+			NPCID.Sets.NPCBestiaryDrawModifiers value = new NPCID.Sets.NPCBestiaryDrawModifiers(0) {
+				Hide = true // Hides this NPC from the Bestiary, useful for multi-part NPCs whom you only want one entry.
+			};
+			NPCID.Sets.NPCBestiaryDrawOffset.Add(NPC.type, value);
+		}
+
+		public override void SetDefaults() {
+			NPC.CloneDefaults(NPCID.TheDestroyerTail);
+			NPC.aiStyle = -1;
+			NPC.defense = 100;
+			NPC.lifeMax = 7907905;
+			NPC.damage = 144;
+			NPC.width = 30; // The width of the npc's hitbox (in pixels)
+			NPC.height = 48; // The height of the npc's hitbox (in pixels)
+			NPC.alpha = 0;
+		}
+
+		public override void Init() {
+			WormBossHead.CommonWormInit(this);
+		}
+
+		private int attackCounter;
+		public override void SendExtraAI(BinaryWriter writer) {
+			writer.Write(attackCounter);
+		}
+
+		public override void ReceiveExtraAI(BinaryReader reader) {
+			attackCounter = reader.ReadInt32();
+		}
+
+		public override void AI() {
+			if (Main.netMode != NetmodeID.MultiplayerClient) {
+				if (attackCounter > 0) {
+					attackCounter--; // tick down the attack counter.
+				}
+
+				Player target = Main.player[NPC.target];
+				// If the attack counter is 0, this NPC is less than 12.5 tiles away from its target, and has a path to the target unobstructed by blocks, summon a projectile.
+				if (attackCounter <= 0 && Vector2.Distance(NPC.Center, target.Center) < 200 && Collision.CanHit(NPC.Center, 1, 1, target.Center, 1, 1)) {
+					Vector2 direction = (target.Center - NPC.Center).SafeNormalize(Vector2.UnitX);
+					direction = direction.RotatedByRandom(MathHelper.ToRadians(10));
+
+					int projectile = Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center, direction * 1, ProjectileID.DeathLaser, 95, 0, Main.myPlayer);
+					Main.projectile[projectile].timeLeft = 60;
+					attackCounter = 250;
+					NPC.netUpdate = true;
+				}
+			}
+		}
+	}
+}
